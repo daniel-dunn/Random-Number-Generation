e8a1d533bf581f8fded5bcab849d46eb260842dc
Daniel <daniel.dunn@kcl.ac.uk>
2021-08-26 00:24:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using CsvHelper;
+using System.Collections;
+using System.IO;
+using System.Data;
+
+namespace RNG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*for (int i = 0; i < 9; i++)
+            {
+                Cenario();
+            }
+            */
+            ArrayList testSet = new ArrayList();
+
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                testSet.Add(r.Next(1, 10));
+            }
+            WriteToCSV(testSet);
+
+        }
+                
+        
+        public static int DRoot(int input)
+        {
+            if (input >= 10)
+            {
+                input = 1 +(input % 10);
+                DRoot(input);
+            }
+            return input;
+        }
+        public static ArrayList RNGL2(ArrayList input)
+        {
+            ArrayList output = new ArrayList();
+
+            foreach (int i in input)
+            {
+                Random s = new Random();
+                if (s.Next(1, 10) > i)
+                {
+                  output.Add(DRoot(i + s.Next(1, 10)));
+                }
+                else
+                {
+                  output.Add(i);
+                }
+            }
+            return output;
+        }
+        public static void PrintA(ArrayList input)
+        {
+            foreach(int i in input)
+            {
+                Console.Write(i +" ");
+            }
+            Console.WriteLine();
+        }
+        public static void Cenario()
+        {
+            Random randNum = new Random();
+            ArrayList dataset = new ArrayList();
+            for (int i = 0; i < 9; i++)
+            {
+                dataset.Add(randNum.Next(1, 10));
+            }
+            Console.WriteLine("dataset");
+            PrintA(dataset);
+            Console.WriteLine("dataset_modified");
+            PrintA(RNGL2(dataset));
+        }
+        public static void AddCSV(ArrayList Number, string filepath)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath, true))
+                {
+                    file.Write(Number);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new ApplicationException("Oopsie", ex);
+            }
+        }
+    
+        public async static void WriteToCSV(ArrayList printArray)
+        {
+
+            String filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            using StreamWriter streamWriter = new StreamWriter(Path.Combine(filePath,"RNGOutput.csv"));
+            foreach (int i in printArray)
+            {
+               await streamWriter.WriteAsync(i + ",");
+            }
+        }  
+    }
+}
